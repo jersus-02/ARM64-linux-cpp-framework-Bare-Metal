b0c92ec5ed015b40d786646f6136c3e469381a68
jersus-02 <jersusgrajaloos@gmail.com>
2026-07-30 15:22:46 -0500

    git did this

--- Resources/string.hpp
+++ Resources/string.hpp
@@ -1,8 +1,14 @@
 #ifndef STRING_HPP
 #define STRING_HPP
-
+class string;
 namespace string_necessities {
-    
+    class chptr {
+        private:
+        char arData[4100] = {0};
+        const char* data = arData;
+        public:
+        friend class string;
+    };
 }
 
 #endif //STRING_HPP
